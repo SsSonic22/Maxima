42de3f82fccabeee2ed093c0f8908581a2fd32be
s <osa@ufin.online>
2023-05-13 17:48:01 +0300

    раз

---
+++ Домашка от 11.04 пули патроны стреляю/Plasma.cs
@@ -0,0 +1,6 @@
+namespace Домашка_от_11._04_пули_патроны_стреляю;
+
+public class Plasma : RailGunBullet
+{
+
+}
---
+++ Домашка от 11.04 пули патроны стреляю/RailGunBullet.cs
@@ -0,0 +1,7 @@
+namespace Домашка_от_11._04_пули_патроны_стреляю;
+
+public class RailGunBullet
+{
+    public int Heat   { get; }
+    public int Damage { get; }
+}
