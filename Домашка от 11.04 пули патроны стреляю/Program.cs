6e474764edb7646d3b28a060f60f2951ae98e390
s <osa@ufin.online>
2023-05-14 11:58:39 +0300

    ...

---
+++ Домашка от 11.04 пули патроны стреляю/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Домашка_от_11._04_пули_патроны_стреляю;
+
+public class Program
+{
+    
+    public void TestRifle<T>(RailGun railgun)
+    {
+        
+    }
+    static void Main()
+    {
+        
+    }
+}
