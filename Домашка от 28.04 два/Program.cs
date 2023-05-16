1e76851915f0a2d5c29c7de0d2ad9137b1dc7d91
s <osa@ufin.online>
2023-05-16 11:44:15 +0300

    --

---
+++ Домашка от 28.04 два/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// Даны целые числа K1 и K2 и целочисленные последовательности A и B. Получить последовательность,
+// содержащую все числа из A, большие K1, и все числа из B, меньшие K2. Отсортировать полученную
+// последовательность по возрастанию.
+    
+public class Program
+{
+    public static void Main()
+    {
+        int K1 = 15;
+        int K2 = 25;
+        
+        var A = new List<int> { 1, 5, 2, 3, 4, 45, 6, 7, 8, 10, 11, 26, 13, 12, 14, 15, 18, 20, 21 };
+        var B = new List<int> { 1, 8, 4, 77, 23, 4, 3, 4, 9, 6, 21, 8, 9, 8, 10, 11, 35, 13, 12, 16, 19, 18, 83, 29, 21 };
+
+        var result = A.Where(x => x > K1)
+                .Concat(B.Where(x => x < K2))
+                .OrderBy(x => x)
+                .ToList();
+        
+        foreach (var i in result)
+ 
+            Console.WriteLine($"{i}");
+        
+        Console.ReadKey();
+    }
+}
---
+++ Домашка от 28.04 три/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+// Исходная последовательность содержит сведения об абитуриентах. Каждый элемент
+// последовательности включает следующие поля:
+// <Фамилия> <Год поступления> <Номер школы>
+// Для каждой школы вывести общее число абитуриентов за все годы и фамилию первого из абитуриентов
+// этой школы, содержащихся в исходном наборе данных (вначале указывать номер школы, затем число
+// абитуриентов, затем фамилию). Сведения о каждой школе выводить на новой строке и упорядочивать
+// по возрастанию номеров школ.
+
+using Домашка_от_28._04_три;
+
+public class Program
+{
+    public static void Main()
+    {
+        var snum1201 = 1201;
+        var snum1503 = 1503;
+        var snum1464 = 1464;
+        var snum1799 = 1799;
+        var snum1099 = 1099;
+        var snum1807 = 1807;
+
+
+        var enrollee1 = new Enrollee("Yankov", 1998, snum1201);
+        var enrollee2 = new Enrollee("Petrov", 2000, snum1503);
+        var enrollee3 = new Enrollee("Pachuli", 2010, snum1464);
+        var enrollee4 = new Enrollee("Sashin", 2006, snum1799);
+        var enrollee5 = new Enrollee("Osipova", 2007, snum1201);
+        var enrollee6 = new Enrollee("Tereshko", 1990, snum1099);
+        var enrollee7 = new Enrollee("Mamaeva", 1999, snum1807);
+        var enrollee8 = new Enrollee("Lyashin", 2003, snum1807);
+        var enrollee9 = new Enrollee("Ivanov", 2003, snum1799);
+        var enrollee10 = new Enrollee("Linin", 2000, snum1464);
+        var enrollee11 = new Enrollee("Chukareva", 2007, snum1099);
+        var enrollee12 = new Enrollee("Borodkin", 2007, snum1201);
+        var enrollee13 = new Enrollee("Azuli", 2013, snum1099);
+        var enrollee14 = new Enrollee("Zubin", 2009, snum1503);
+        var enrollee15 = new Enrollee("Zazulya", 1996, snum1201);
+        var enrollee16 = new Enrollee("Petrovin", 2000, snum1503);
+        var enrollee17 = new Enrollee("Melnik", 2010, snum1464);
+        var enrollee18 = new Enrollee("Friskin", 2006, snum1799);
+        var enrollee19 = new Enrollee("Tereshenok", 2007, snum1201);
+        var enrollee20 = new Enrollee("Minaev", 1990, snum1099);
+        var enrollee21 = new Enrollee("Shatohina", 1999, snum1807);
+        var enrollee22 = new Enrollee("Minin", 2003, snum1807);
+        var enrollee23 = new Enrollee("Pozharskyi", 2003, snum1799);
+        var enrollee24 = new Enrollee("Chestnyi", 2000, snum1464);
+        var enrollee25 = new Enrollee("Khokhlov", 2007, snum1099);
+        var enrollee26 = new Enrollee("Borodin", 2007, snum1099);
+        var enrollee27 = new Enrollee("Drunina", 2013, snum1201);
+        var enrollee28 = new Enrollee("Latinina", 2009, snum1503);
+        
+        
+    }
+}
