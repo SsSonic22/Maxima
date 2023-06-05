76744a787d65cbc201e29545247f65e473dc3aba
s <osa@ufin.online>
2023-06-05 20:29:49 +0300

    я доделал я доделал

---
+++ Переучиваю тему тасок/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class Program
+{
+    private static object _sync = new object();
+
+    static void Main(string[] args)
+    {
+
+        var task = new Task(PrintTimeAction);
+        task.Start();
+        
+        while (true)
+        {
+            Console.WriteLine("Enter numbers");
+
+            Console.ReadLine();
+            
+            lock (_sync)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine($"SUM is : {Calculate(a, b)}");
+            }
+        }
+    }
+
+    private static void PrintTimeAction()
+    {
+        while (true)
+        {
+            lock (_sync)
+            {
+                Console.WriteLine($"TIME: {DateTime.Now:t}");
+            }
+            Thread.Sleep(3000);
+        }
+    }
+    private static int Calculate(int a, int b)
+    {
+        return a + b;
+    }
+}
